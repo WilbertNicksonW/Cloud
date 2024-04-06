7d9a7c6fc63a37d4847ae4d5ae89224a4520f80f
WilbertNicksonW <149926462+WilbertNicksonW@users.noreply.github.com>
2024-04-07 00:14:34 +0800

    app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Wilbert Nickson Winarto <br> 51021007 <br> Sistem Informasi ')
 })
 
 app.listen(PORT, HOST)
